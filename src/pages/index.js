4e70a728863ee5afb01a780703c09c690706ff60
Noor Mohammed <noormohammedknr2@gmail.com>
2023-04-14 21:05:44 +0530

    updated the redux

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>Hello from the Next Js APP</>;
+  return (
+    <div className=" bg-slate-700 text-left text-black">
+      Hello from the Next js app
+    </div>
+  );
 }
